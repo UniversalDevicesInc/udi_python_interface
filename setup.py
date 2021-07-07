c71b6bb2b47b0adc060ef10b064926e148711cfd
Bob Paauwe <bpaauwe@yahoo.com>
2021-07-07 23:04:56 +0000

    Changing which python versions are valid.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='udi_interface',
         "netifaces",
         "pyisy"
     ],
-    python_requires='>2.7,!=3.0.*,!=3.1.*,!=3.2.*',
+    python_requires='>3.6.*',
     zip_safe=False,
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -35,10 +35,8 @@ setup(name='udi_interface',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9'
     ])
