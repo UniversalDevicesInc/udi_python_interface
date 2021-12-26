e1675678364ec6529c98c4345612ea2ca396c16c
Bob Paauwe <bpaauwe@yahoo.com>
2021-12-25 16:22:16 -0800

    Version 3.0.26

--- udi_interface/__init__.py
+++ udi_interface/__init__.py
@@ -7,7 +7,7 @@ from .interface import Interface
 from .custom import Custom
 from .isy import ISY
 
-__version__ = '3.0.25'
+__version__ = '3.0.26'
 __description__ = 'UDI Python Interface for Polyglot version 3'
 __url__ = 'https://github.com/UniversalDevicesInc/udi-python-interface'
 __author__ = 'Universal Devices Inc.'
