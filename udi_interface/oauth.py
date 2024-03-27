cfbb7f7234fe33b31cd02cc3fd90237bf5cbec9e
bmercier <benoit@universal-devices.com>
2024-03-27 20:48:49 -0300

    Removed test code that refreshed the oAuth tokens every times

--- udi_interface/oauth.py
+++ udi_interface/oauth.py
@@ -115,7 +115,7 @@ class OAuth:
             expiry = self._oauthTokens.get('expiry')
 
             # If expired or expiring in less than 60 seconds, refresh
-            if (expiry is None or datetime.fromisoformat(expiry) - timedelta(seconds=60) < datetime.now()) or True:
+            if expiry is None or datetime.fromisoformat(expiry) - timedelta(seconds=60) < datetime.now():
 
                 LOGGER.info(f"Access tokens: Token is expired since { expiry }. Initiating refresh.")
                 self._oAuthTokensRefresh()
